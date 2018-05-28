290d03953adafff02e8dee4682114c1a853492af
Amos Wenger <amoswenger@gmail.com>
2018-05-28 12:38:16 +0200

    resumable upload tests

---
+++ uploader/resumable_test.go
@@ -0,0 +1,149 @@
+package uploader
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/itchio/httpkit/progress"
+	"github.com/itchio/savior/fullyrandom"
+	"github.com/itchio/wharf/state"
+	"github.com/itchio/wharf/wtest"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ChunkUploader(t *testing.T) {
+	loudTests := os.Getenv("LOUD_TESTS") == "1"
+	log := func(format string, a ...interface{}) {
+		if loudTests {
+			log.Printf(format, a...)
+		} else {
+			t.Logf(format, a...)
+		}
+	}
+
+	server := makeTestServer(t, log)
+	server.settings.latency = 200 * time.Millisecond
+	server.settings.bandwidthBytesPerSec = 10 * 1024 * 1024 // 10 MB/s
+	ru := NewResumableUpload(server.URL)
+	ru.SetConsumer(&state.Consumer{
+		OnMessage: func(lvl string, msg string) {
+			log("[%s] %s", lvl, msg)
+		},
+	})
+
+	ref := new(bytes.Buffer)
+	mw := io.MultiWriter(ref, ru)
+
+	wtest.Must(t, fullyrandom.Write(mw, 16*1024*1024, time.Now().UnixNano()))
+	wtest.Must(t, ru.Close())
+
+	assert.EqualValues(t, ref.Bytes(), server.state.data)
+	log("num blocks stored: %+v", server.state.numBlocksStored)
+}
+
+type fakeGCS struct {
+	*httptest.Server
+	state struct {
+		data            []byte
+		head            int64
+		numBlocksStored []int64
+	}
+	settings struct {
+		latency              time.Duration
+		bandwidthBytesPerSec int64
+	}
+}
+
+func makeTestServer(t *testing.T, log func(msg string, a ...interface{})) *fakeGCS {
+	fg := &fakeGCS{}
+
+	var chunkSize int64 = 256 * 1024
+
+	fg.Server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if fg.settings.latency > 0 {
+			log("Sleeping %s (to simulate latency)", fg.settings.latency)
+			time.Sleep(fg.settings.latency)
+		}
+
+		switch r.Method {
+		case "PUT":
+			log("Putting...")
+			contentRange := r.Header.Get("content-range")
+			if !strings.HasPrefix(contentRange, "bytes ") {
+				w.WriteHeader(400)
+				fmt.Fprintf(w, "Missing 'bytes ' prefix in content-range header")
+				return
+			}
+
+			contentRange = strings.TrimPrefix(contentRange, "bytes ")
+
+			log("contentRange: %s", contentRange)
+			slashTokens := strings.Split(contentRange, "/")
+			storedString := slashTokens[0]
+			totalString := slashTokens[1]
+
+			storedTokens := strings.SplitN(storedString, "-", 2)
+			start, err := strconv.ParseInt(storedTokens[0], 10, 64)
+			wtest.Must(t, err)
+			end, err := strconv.ParseInt(storedTokens[1], 10, 64)
+			wtest.Must(t, err)
+			end++
+
+			sentBytes := int64(end - start)
+			if sentBytes%chunkSize != 0 {
+				w.WriteHeader(400)
+				fmt.Fprintf(w, "Sent bytes (%d) were not a multiple of chunk size (%d)", sentBytes, chunkSize)
+				return
+			}
+
+			total, _ := strconv.ParseInt(totalString, 10, 64)
+
+			log("start=%d, end=%d, total=%d", start, end, total)
+			committedRange := &httpRange{
+				start: 0,
+				end:   end,
+			}
+			w.Header().Set("range", committedRange.String())
+
+			if totalString != "*" {
+				log("last block!")
+				w.WriteHeader(200)
+			} else {
+				log("committing blocks...")
+				w.WriteHeader(308)
+			}
+
+			defer r.Body.Close()
+			buf, err := ioutil.ReadAll(r.Body)
+			wtest.Must(t, err)
+			fg.state.data = append(fg.state.data, buf...)
+			fg.state.head += int64(len(buf))
+			fg.state.numBlocksStored = append(fg.state.numBlocksStored, sentBytes/chunkSize)
+
+			if fg.settings.bandwidthBytesPerSec > 0 {
+				bps := fg.settings.bandwidthBytesPerSec
+				sleepDuration := time.Millisecond * time.Duration(float64(sentBytes)/float64(bps)*1000.0)
+				log("Sleeping %s (to simulating %s bandwidth)", sleepDuration, progress.FormatBPS(bps, time.Second))
+				time.Sleep(sleepDuration)
+			}
+
+			return
+		default:
+			log("Dunno what to do with request: %#v", r)
+			w.WriteHeader(400)
+			return
+		}
+	}))
+
+	return fg
+}
